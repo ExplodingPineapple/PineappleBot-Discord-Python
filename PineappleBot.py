f86e517cfb2a3a61547d49cd5aa90bc051f52130
Deanosim1 <Deanosim1@explodingpineapple.com>
2017-06-22 00:01:46 +1000

    Commit PineappleBot.py

---
+++ PineappleBot.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+description = 'PineappleBot the Best Discord Bot!'
+bot_prefix = '!'
+
+client = commands.Bot(description=description, command_prefix=bot_prefix)
+
+@client.event
+async def on_ready():
+    print('Logged In')
+    print('Name : {}'.format(client.user.name))
+    print('ID : {}'.format(client.user.id))
+    print(discord.__version__)
+
+    @client.command(pass_context=True)
+    async def ping(ctx):
+        await client.say('Pong!')
