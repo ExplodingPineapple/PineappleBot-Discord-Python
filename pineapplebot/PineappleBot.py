6dc461503dbd3c23513d9d5c20da2be5040f2013
William Lees <willwill56@gmail.com>
2017-06-23 01:37:18 +1000

    Added adding commands

--- pineapplebot/PineappleBot.py
+++ pineapplebot/PineappleBot.py
@@ -19,14 +19,29 @@ async def on_ready():
     print('Discord Library : {}'.format(discord.__version__))
     print('Python Version : {}'.format(sys.version))
 
-    @client.command(pass_context=True)
-    async def ping(ctx):
-        await client.say('Pong!')
-        await client.say(ctx.message.author.mention + ' I\'m watching you buddy!')
+@client.command(pass_context=True)
+async def ping(ctx):
+    await client.say('Pong!')
+    await client.say(ctx.message.author.mention + ' I\'m watching you buddy!')
 
-    @client.command(pass_context=True)
-    async def version(ctx):
-        await client.say('PineappleBot version : {}'.format(bot_version))
-        await client.say('Running on Python version : {}'.format(sys.version))
+@client.command(pass_context=True)
+async def version(ctx):
+    await client.say('PineappleBot version : {}'.format(bot_version))
+    await client.say('Running on Python version : {}'.format(sys.version))
+
+@client.command()
+async def addadd(text: str):
+    """Adds Add to the input text."""
+    await client.say("Add " + text)
+
+@client.command()
+async def addnaddadd(left: int, right: int):
+    """Adds two numbers together and adds Add."""
+    await client.say("Add " + str(left + right))
+
+@client.command()
+async def addnaddaddnadd(left: int, right: int, text: str):
+    """Adds two numbers together and adds Add, then adds that to the input text."""
+    await client.say("Add {0} {1}".format(str(left + right), text))
 
 client.run('token')
