1091f515544200b31fd1c6ac81fd1ca57f58a8e5
William Lees <willwill56@gmail.com>
2017-06-22 23:35:43 +1000

    move version command back to main module

--- pineapplebot/PineappleBot.py
+++ pineapplebot/PineappleBot.py
@@ -7,6 +7,7 @@ from discord.ext import commands
 
 DESCRIPTION = 'PineappleBot the Best Discord Bot!'
 BOT_PREFIX = '!'
+BOT_VERSION = '0.0.1 alpha'
 
 logging.basicConfig(level=logging.INFO)
 
@@ -20,11 +21,14 @@ async def on_ready():
     print('Discord Library : {}'.format(discord.__version__))
     print('Python Version : {}'.format(sys.version))
 
-    @client.command(pass_context=True)
-    async def ping(ctx):
-        await client.say('Pong!')
-        await client.say(ctx.message.author.mention + ' I\'m watching you buddy!')
+@client.command(pass_context=True)
+async def ping(ctx):
+    await client.say('Pong!')
+    await client.say(ctx.message.author.mention + ' I\'m watching you buddy!')
 
-
+@client.command(pass_context=True)
+async def version(ctx):
+    await client.say('PineappleBot version : {}'.format(BOT_VERSION))
+    await client.say('Running on Python version : {}'.format(sys.version))
 
 client.run(config.bot_token)
