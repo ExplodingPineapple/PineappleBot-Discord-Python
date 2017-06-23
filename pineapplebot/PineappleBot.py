48c3ee50128d87aad3228d2a093bea59bc52ea6d
Deanosim1 <Deanosim1@explodingpineapple.com>
2017-06-24 00:45:58 +1000

    Added count command

--- pineapplebot/PineappleBot.py
+++ pineapplebot/PineappleBot.py
@@ -2,6 +2,7 @@ import sys
 import logging
 import random
 import asyncio
+import time
 import config
 import plugins.stream_alerts
 import discord
@@ -17,6 +18,7 @@ bot = commands.Bot(description=DESCRIPTION, command_prefix=BOT_PREFIX)
 
 @bot.event
 async def on_ready():
+    """Prints to terminal when bot is ready"""
     print('Logged In')
     print('Name : {}'.format(bot.user.name))
     print('ID : {}'.format(bot.user.id))
@@ -25,11 +27,13 @@ async def on_ready():
 
 @bot.command(pass_context=True)
 async def ping(ctx):
+    """Simple ping pong command to test the bot"""
     await bot.say('Pong!')
     await bot.say(ctx.message.author.mention + ' I\'m watching you buddy!')
 
 @bot.command(pass_context=True)
-async def version(ctx):
+async def version():
+    """Replys in chat with bot's version info"""
     await bot.say('PineappleBot version : {}'.format(BOT_VERSION))
     await bot.say('Running on Python version : {}'.format(sys.version))
 
@@ -38,5 +42,14 @@ async def add(left: int, right: int):
     """Adds two numbers together."""
     await bot.say(left + right)
 
+@bot.command()
+async def count():
+    """Counts to ten."""
+    COUNT = 1
+    while COUNT <= 5:
+        await bot.say(COUNT)
+        COUNT = COUNT + 1
+        time.sleep(1)
+    await bot.say("Finished!")
 
 bot.run(config.bot_token)
